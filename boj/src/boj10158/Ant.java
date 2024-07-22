e2011e736256b8717d2b4d068c85a6b0556d8ada
seohye-ki <seohyeon.shkim@gmail.com>
2024-07-22 11:28:48 +0900

    mod: boj10158

--- boj/src/boj10158/Ant.java
+++ boj/src/boj10158/Ant.java
@@ -1,38 +1,45 @@
 package boj10158;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Ant {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int width = sc.nextInt();
-		int height = sc.nextInt();
-		int x = sc.nextInt();
-		int y = sc.nextInt();
-		int time = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+//		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String tmp = br.readLine();
+		int width = Integer.parseInt(tmp.split(" ")[0]);
+		int height = Integer.parseInt(tmp.split(" ")[1]);
+		tmp = br.readLine();
+		int x = Integer.parseInt(tmp.split(" ")[0]);
+		int y = Integer.parseInt(tmp.split(" ")[1]);
+		int time = Integer.parseInt(br.readLine());
 		
 		int tmpX = time % (width * 2);
 		int tmpY = time % (height * 2);
-		
-		System.out.println("tmpX " + tmpX);
-		System.out.println("tmpY " + tmpY);
-		
-		if(x + tmpX > width) {
-			x = tmpX - (width - x);
-			if(x >= width)
-				x = width - x;			
-		}
-		else
-			x += tmpX;
-		if(y + tmpY > height) {
-			y = tmpY - (height - y);
-			if(y >= height)
-				y = height - y;			
-		}
-		else
-			y += tmpY;
 
-		System.out.println(x + " " + y);
-		sc.close();
+		if(x + tmpX >= (width * 2))
+			x = (x + tmpX) - (width * 2);
+		else if(x + tmpX >= width)
+			x = (width * 2) - (x + tmpX);
+		else
+			x = x + tmpX;
+		if(y + tmpY >= (height * 2))
+			y = (y + tmpY) - (height * 2);
+		else if(y + tmpY >= height)
+			y = (height * 2) - (y + tmpY);
+		else
+			y = y + tmpY;
+		
+		bw.write(x + " " + y);
+		bw.flush();
+		bw.close();
+		br.close();
+//		System.out.println(x + " " + y);
+//		sc.close();
 	}
 }
