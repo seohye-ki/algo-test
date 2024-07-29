73ec2a5ef19c91a3e72bd7aa00b0cbb540a2fb74
seohye-ki <seohyeon.shkim@gmail.com>
2024-07-29 17:53:00 +0900

    confetti2 copy

--- boj/src/boj2567/Confetti2Copy.java
+++ boj/src/boj2567/Confetti2Copy.java
@@ -3,7 +3,7 @@ package boj2567;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Confetti2 {
+public class Confetti2Copy {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		//100 * 100 도화지
@@ -11,33 +11,27 @@ public class Confetti2 {
 		for(int i = 0; i < 100; i++)
 			Arrays.fill(paper[i], 4);
 		
-		//중복값 체크 배열
-		int[][] check = new int[100][100];
-		for(int i = 0; i < 100; i++)
-			Arrays.fill(check[i], 0);
-		
 		int N = sc.nextInt();
 		for(int t = 0;  t < N; t++) {
 			int x = sc.nextInt();
 			int y = sc.nextInt();
-			//전에 나왔던 값인지 확인
-			if(check[y][x] == 1)
-				continue;
-			else
-				check[y][x] = 1;
-			
+
 			//빈공간: 4, 테두리: 3, 꼭짓점: 2, 변: 1, 면: 0
 			for(int r = 99 - y; r >= 100 - y - 10; r--) {
 				for(int c = x; c < x + 10; c++) {
-					if(paper[r][c] == 0) //이미 색종이가 붙어있는 곳이라면 볼필요 없음
+					 //이미 색종이가 붙어있는 곳이라면 볼필요 없음
+					if(paper[r][c] == 0)
 						continue;
+					
 					//꼭짓점일때 왼쪽하단, 왼쪽상단, 오른쪽하단, 오른쪽상단
 					if((r == 99 - y && c == x) || (r == 100 - y - 10 && c == x) ||
 						(r == 99 - y && c == x + 9) || (r == 100 - y - 10 && c == x + 9)) {
-						if(paper[r][c] == 4)//빈공간이면
+						//빈공간에 꼭짓점을 할당될때
+						if(paper[r][c] == 4)
 							paper[r][c] = 2;
+						
+						//테두리에 꼭짓점을 할당할때(인접한곳에 변이나 꼭짓점이 있다면 (-1)도 해주기)
 						else if(paper[r][c] == 3) {
-							//인접한곳에 변이나 꼭짓점이 있다면 (- 1) 해주기
 							//왼쪽하단
 							if(r == 99 - y && c == x) {
 								paper[r][c] = 2;
@@ -95,8 +89,57 @@ public class Confetti2 {
 								}
 							}
 						}
-						else
-							paper[r][c] = 0;
+						//변이나 꼭짓점에 꼭짓점이 할당될때
+						else {
+							//왼쪽하단
+							if(r == 99 - y && c == x) {
+								//좌 & 하
+								if(c < 0 || (c - 1 >= 0 && paper[r][c - 1] == 3) || r + 1 >= 100 || (r + 1 < 100 && paper[r + 1][c] == 3)) {
+									if(paper[r][c] == 1)
+										paper[r][c] = 1;
+									else
+										paper[r][c] = 2;
+								}
+								else
+									paper[r][c] = 0;
+							}
+							//왼쪽상단
+							else if(r == 100 - y - 10 && c == x) {
+								//상 & 좌
+								if(r < 0 || (r - 1 >= 0 && paper[r - 1][c] == 3) || c < 0 || (c - 1 >= 0 && paper[r][c - 1] == 3)) {
+									if(paper[r][c] == 1)
+										paper[r][c] = 1;
+									else
+										paper[r][c] = 2;
+								}
+								else
+									paper[r][c] = 0;
+							}
+							//오른쪽하단
+							else if(r == 99 - y && c == x + 9) {
+								//우 & 하
+								if(c >= 100 || (c + 1 < 100 && paper[r][c + 1] == 3) || r >= 100 || (r + 1 < 100 && paper[r + 1][c] == 3)) {
+									if(paper[r][c] == 1)
+										paper[r][c] = 1;
+									else
+										paper[r][c] = 2;
+								}
+								else
+									paper[r][c] = 0;
+							}
+							//오른쪽 상단
+							else {
+								//우 & 상
+								if(c >= 100 || (c + 1 < 100 && paper[r][c + 1] == 3) || r < 0|| (r - 1 >= 0 && paper[r - 1][c] == 3)) {
+									if(paper[r][c] == 1)
+										paper[r][c] = 1;
+									else
+										paper[r][c] = 2;
+								}
+								else
+									paper[r][c] = 0;
+							}
+						}
 					}
 					//변일때 아랫변, 윗변, 왼변, 오른변
 					else if(r == 99 - y || r == 100 - y - 10 || c == x || c == x + 9) {
